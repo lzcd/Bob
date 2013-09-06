4c4d08bdf91615dffe06cb6021f2198c04a7733c
lzcd <lzcd@hotmail.com>
2013-09-06 14:42:09 +1000

    Inital solution

---
+++ Tests/GeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bob;
+
+namespace Tests
+{
+    [TestClass]
+    public class GeneratorTests
+    {
+        [TestMethod]
+        public void CanGenerateWordOfSetLength()
+        {
+            var generator = new Generator();
+            var name = generator.Generate(5);
+            Assert.AreEqual(5, name.Length);
+        }
+
+        [TestMethod]
+        public void Sandpit()
+        {
+            var generator = new Generator();
+
+            var emailAddress = generator.Generate(3, 10) + "." + generator.Generate(3, 10) + "@" + generator.Generate(3, 10) + ".com";
+        }
+    }
+}
